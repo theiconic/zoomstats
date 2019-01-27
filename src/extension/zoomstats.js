5c293bfa1139a00d370eae0af5f5851e7a76d1af
Andre Wyrwa <andre.wyrwa@theiconic.com.au>
2019-01-28 04:29:30 +1100

    Remove leftover console.logs

--- src/extension/zoomstats.js
+++ src/extension/zoomstats.js
@@ -2,14 +2,10 @@ const parser = require('../parser/html.js');
 const Messenger = require('ext-messenger');
 const messenger = new Messenger();
 
-console.log('huhu');
-
 messenger.initConnection('main', (msg, from, sender, sendResponse) => {
     if (msg.cmd === 'getTranscript') {
         const dom = window.document;
         const data = parser.parse(dom);
-
-        console.log(data);
 
         sendResponse(data);
     } else {
