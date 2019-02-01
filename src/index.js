61fab0075ee156e72fdfd0470cfdea2aa12d43df
Andre Wyrwa <andre.wyrwa@theiconic.com.au>
2019-02-02 04:44:14 +1100

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const jsdom = require('jsdom');
-const parse = require('./parser/html.js');
 const talktime = require('./analyzer/talktime.js');
 const effectiveness = require('./analyzer/effectiveness.js');
 const words = require('./analyzer/words.js');
 const topwords = require('./renderer/json/topwords.js');
-const { JSDOM } = jsdom;
+const parseHtmlFile = require('./util/parse-html-file');
+const print = require('./util/print-to-stdout');
 
 program
     .command('talktime <file>')
     .description('Analyze the given recording HTML file for talktime per speaker')
     .action(file => {
-        console.log('analyzing file %s', file);
+        print('analyzing file %s', file);
 
-        JSDOM.fromFile(file, {}).then(dom => {
-            const data = parse(dom.window.document);
-            const times = talktime(data);
-
-            console.log(times);
+        parseHtmlFile(file).then(data => {
+            print(talktime(data));
         });
     });
 
@@ -27,12 +23,10 @@ program
     .command('effectiveness <file>')
     .description('Analyze the given recording HTML file for overall meeting effectiveness')
     .action(file => {
-        console.log('analyzing file %s', file);
+        print('analyzing file %s', file);
 
-        JSDOM.fromFile(file, {}).then(dom => {
-            const data = parse(dom.window.document);
-
-            console.log('Meeting effectiveness: ' + effectiveness(data) + '%');
+        parseHtmlFile(file).then(data => {
+            print('Meeting effectiveness: ' + effectiveness(data) + '%');
         });
     });
 
@@ -40,12 +34,10 @@ program
     .command('topwords <file>')
     .description('Analyze the given recording HTML file for top words')
     .action(file => {
-        console.log('analyzing file %s', file);
+        print('analyzing file %s', file);
 
-        JSDOM.fromFile(file, {}).then(dom => {
-            const data = parse(dom.window.document);
-
-            console.log(topwords(words(data)));
+        parseHtmlFile(file).then(data => {
+            print(topwords(words(data)));
         });
     });
 
