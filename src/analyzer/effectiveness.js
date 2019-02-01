9d0ec16181b23d7f63c81920e924a1ef016fb277
Andre Wyrwa <andre.wyrwa@theiconic.com.au>
2019-02-02 04:56:40 +1100

    Refactor effectiveness analyzer

--- src/analyzer/effectiveness.js
+++ src/analyzer/effectiveness.js
@@ -1,12 +1,13 @@
 const moment = require('moment');
 const getTalkTime = require('./util/extract-talktimes');
 const momentify = require('./util/momentify-zoom-timestamp');
+const roundTo = require('./util/round-to');
+const roundToTwoDigits = roundTo(2);
+const extractTalkTime = getTalkTime();
 
 module.exports = data => {
-    const effectiveTime = data.transcript.reduce(getTalkTime(), 0);
+    const effectiveTime = data.transcript.reduce(extractTalkTime, 0);
+    const totalTime = momentify(data.duration).diff(momentify(0));
 
-    const total = momentify(data.duration)
-        .diff(momentify(0));
-
-    return Math.round((effectiveTime / total * 100) * 100) / 100;
+    return roundToTwoDigits(effectiveTime / totalTime * 100);
 }
