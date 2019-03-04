510bbebfa71fec990c9dfc6ebd2265cc26ac6e47
Andre Wyrwa <andre.wyrwa@theiconic.com.au>
2019-03-04 18:09:31 +1100

    Add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    testPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+    ],
+    coverageReporters: [
+        "lcov"
+    ],
+    testMatch: [
+        "<rootDir>/src/**/*.test.js"
+    ],
+    collectCoverageFrom: [
+        'src/**/*.js',
+    ],
+    moduleFileExtensions: [
+        'js'
+    ],
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    testURL: 'http://localhost'
+}
